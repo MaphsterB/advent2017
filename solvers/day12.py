01b259e15804f6257b5f76803580c06893ae9cb9
MaphsterB <docmaph@gmail.com>
2017-12-12 17:52:00 -0500

    Expand Day 12 docstrings

--- solvers/day12.py
+++ solvers/day12.py
@@ -48,6 +48,22 @@ Therefore, a total of 6 programs are in this group; all but program 1, which
 has a pipe that connects it to itself.
 
 How many programs are in the group that contains program ID 0?
+
+--- Part Two ---
+
+There are more programs than just the ones in the group containing program ID
+0. The rest of them have no way of reaching that group, and still might have no
+way of reaching each other.
+
+A group is a collection of programs that can all communicate via pipes either
+directly or indirectly. The programs you identified just a moment ago are all
+part of the same group. Now, they would like you to determine the total number
+of groups.
+
+In the example above, there were 2 groups: one consisting of programs
+0,2,3,4,5,6, and the other consisting solely of program 1.
+
+How many groups are there in total?
 """
 
 
@@ -55,6 +71,17 @@ import re
 
 
 class PipeGraph:
+    """
+    The graph class used to solve the puzzle.
+    It simply keeps an edge table dict (which would
+    let us complete the problem even if the edge list
+    showed up out-of-order) and knows how to run
+    a variant of Dijkstra's algorithm for visiting
+    nodes until we run out of newly-discovered ones.
+
+    Also can do this for each node in the graph,
+    returning partition sets.
+    """
 
     def __init__(self):
         self.edge_table = {}
