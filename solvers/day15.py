cc3e5bf475486ca73610b6663dd0827232824533
MaphsterB <docmaph@gmail.com>
2017-12-18 10:37:28 -0500

    Refactor Day 15 using actual generators

--- solvers/day15.py
+++ solvers/day15.py
@@ -55,31 +55,103 @@ To get a significant sample, the judge would like to consider 40 million pairs.
 that match in their lowest 16 bits.)
 
 After 40 million pairs, what is the judge's final count?
+
+--- Part Two ---
+
+In the interest of trying to align a little better, the generators get more
+picky about the numbers they actually give to the judge.
+
+They still generate values in the same way, but now they only hand a value to
+the judge when it meets their criteria:
+
+Generator A looks for values that are multiples of 4.
+Generator B looks for values that are multiples of 8.
+
+Each generator functions completely independently: they both go through values
+entirely on their own, only occasionally handing an acceptable value to the
+judge, and otherwise working through the same sequence of values as before
+until they find one.
+
+The judge still waits for each generator to provide it with a value before
+comparing them (using the same comparison method as before). It keeps track of
+the order it receives values; the first values from each generator are
+compared, then the second values from each generator, then the third values,
+and so on.
+
+Using the example starting values given above, the generators now produce the
+following first five values each:
+
+--Gen. A--  --Gen. B--
+1352636452  1233683848
+1992081072   862516352
+ 530830436  1159784568
+1980017072  1616057672
+ 740335192   412269392
+
+These values have the following corresponding binary values:
+
+01010000100111111001100000100100
+01001001100010001000010110001000
+
+01110110101111001011111010110000
+00110011011010001111010010000000
+
+00011111101000111101010001100100
+01000101001000001110100001111000
+
+01110110000001001010100110110000
+01100000010100110001010101001000
+
+00101100001000001001111001011000
+00011000100100101011101101010000
+
+Unfortunately, even though this change makes more bits similar on average, none
+of these values' lowest 16 bits match. Now, it's not until the 1056th pair that
+the judge finds the first match:
+
+--Gen. A--  --Gen. B--
+1023762912   896885216
+
+00111101000001010110000111100000
+00110101011101010110000111100000
+
+This change makes the generators much slower, and the judge is getting
+impatient; it is now only willing to consider 5 million pairs. (Using the
+values from the example above, after five million pairs, the judge would
+eventually find a total of 309 pairs that match in their lowest 16 bits.)
+
+After 5 million pairs, but using this new generator logic, what is the judge's
+final count?
+
 """
 
 
 import re
 
 
-class DuelingGen:
+class DuelingGens:
+    """
+    Class representing a pair of generators creating values
+    in a finite space determined by a modulus.
+    """
 
     MODULUS = 2147483647
     MOD_MASK = 0x7fffffff
     JUDGE_MASK = 0xffff
 
-    def __init__(self, init_val, factor):
-        self.f = factor
-        self.val = init_val
-
-    def gen(self):
-        self.val = (self.val * self.f) % self.MODULUS
-        return self.val
+    @classmethod
+    def gen(cls, init, factor, lim=10):
+        x = init
+        for _ in range(lim):
+            x = (x * factor) % cls.MODULUS
+            yield x
 
     @classmethod
-    def judge(cls, gen1, gen2):
-        val1 = gen1.gen() & cls.JUDGE_MASK
-        val2 = gen2.gen() & cls.JUDGE_MASK
-        return val1 == val2
+    def judge(cls, val1, f1, val2, f2, lim=10):
+        return len([
+            True for (x1, x2) in zip(cls.gen(val1, f1, lim), cls.gen(val2, f2, lim))
+            if x1 & cls.JUDGE_MASK == x2 & cls.JUDGE_MASK
+        ])
 
 
 GEN_A_FACTOR = 16807
@@ -92,12 +164,7 @@ def part1(input_lines):
     """
     init1 = int(re.search(r"(\d+)", input_lines[0]).group(1))
     init2 = int(re.search(r"(\d+)", input_lines[1]).group(1))
-    g1 = DuelingGen(init1, GEN_A_FACTOR)
-    g2 = DuelingGen(init2, GEN_B_FACTOR)
-    count = 0
-    for _ in range(40000000):
-        if DuelingGen.judge(g1, g2): count += 1
-    return count
+    return DuelingGens.judge(init1, GEN_A_FACTOR, init2, GEN_B_FACTOR, 40000000)
 
 
 def part2(input_lines):
